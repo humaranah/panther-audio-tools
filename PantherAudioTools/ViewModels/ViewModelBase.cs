938a4ef3b0251e599c7973c786fc7b2bb81190d4
Humberto Arana <humaranah@outlook.com>
2025-09-08 20:28:18 -0500

    HA: Add base project

---
+++ PantherAudioTools/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PantherAudioTools.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ PantherAudioTools/Views/MainWindow.axaml.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Controls;
+
+namespace PantherAudioTools.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        TransparencyLevelHint =
+        [
+            WindowTransparencyLevel.Mica,
+            WindowTransparencyLevel.AcrylicBlur
+        ];
+        InitializeComponent();
+    }
+}

6bd1646220142f51b8f2473051697f39d3530d5b
Humberto Arana <humaranah@outlook.com>
2026-04-13 09:25:18 -0500

    Integrate Windows 11 visual styles

--- source/Panther.AudioTools/App.xaml.cs
+++ source/Panther.AudioTools/App.xaml.cs
@@ -1,6 +1,5 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Wpf.Ui.Appearance;
 
 namespace Panther.AudioTools
 {
@@ -9,6 +8,10 @@ namespace Panther.AudioTools
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ApplicationThemeManager.ApplySystemTheme();
+        }
     }
-
 }
